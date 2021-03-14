b5bbeae25f1be1e52ca930fa14cd18e9fa8a707d
an-ger <aiboowen@gmail.com>
2021-03-14 23:24:54 +0800

    Koa基础使用

--- day_7_Koa2/k.js
+++ day_7_Koa2/k.js
@@ -1,7 +1,7 @@
 const koa = require('koa')
 const app = new koa();
 app.use((context, next) => {
-    
+    context.response.body = "hello world";
 })
 app.listen(8888, () => {
     console.log('success');
