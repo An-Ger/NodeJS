84c66ce23479e23b2626e762d2426f6c9d78c9ad
an-ger <aiboowen@gmail.com>
2021-03-24 23:08:25 +0800

    Node创建服务器两种方法

--- day_6/创建服务器方式.js
+++ day_6/创建服务器方式.js
@@ -1,11 +1,14 @@
 const http = require('http')
 const server1 = http.createServer((req ,res) => {
+    res.write('success')
     res.end("server1")
-})
-server1.listen(8001, () => {
-    console.log('server1');
 })
 
 const server2 = new http.Server((req, res) => {
-    console.log("server2");
+    res.end("server2");
 });
+
+//启动服务器
+server2.listen(8001, () => {
+    console.log('server1');
+})
